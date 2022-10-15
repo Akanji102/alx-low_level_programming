18e6ae24c740536e5d1a0ea6752e5ed70f1f677d
Fawole Joshua Ajibola <fawolejoshuaajibola@gmail.com>
2022-10-15 06:23:39 +0100

    Task 1:00-00

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 else if (l == 0)
 {
-prinf("Last digit of %d is %d and is 0\n", n, l);
+printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
 {
