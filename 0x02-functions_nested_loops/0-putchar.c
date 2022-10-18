4d94ec752ae6b95300f88078c79c77115c02ca95
Fawole Joshua Ajibola <fawolejoshuaajibola@gmail.com>
2022-10-18 23:15:00 +0100

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*main- Write a program that prints _putchar
+*written by onyemagod
+*Return: always 0
+*/
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+	return (0);
+}i
