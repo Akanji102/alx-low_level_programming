e68ccb9133b12172b912ef90af606ae9b0ba6dc1
Fawole Joshua Ajibola <fawolejoshuaajibola@gmail.com>
2022-10-19 01:07:20 +0100

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ unsigned long fib1 = 0, fib2 = 1, sum;
 unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
 for (count = 0; count < 92; count++)
-{	
+{
 sum = fib1 + fib2;
 printf("%lu, ", sum);
 fib1 = fib2;
