014b2e2e19a1192aaaf19d60b193729348f37068
Fawole Joshua Ajibola <fawolejoshuaajibola@gmail.com>
2022-10-25 01:36:56 +0100

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,8 +13,9 @@ void print_array(int *a, int n)
 {
 int x;
 
-for (x = 0; x < n; x++)if (x != n - 1)
-printf("%d, ", a[x]); 
+for (x = 0; x < n; x++)
+if (x != n - 1)
+printf("%d, ", a[x]);
 else
 printf("%d", a[x]);
 printf("\n");
