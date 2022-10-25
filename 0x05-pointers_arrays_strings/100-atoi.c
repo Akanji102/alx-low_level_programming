ff4e8b0aece5f2b6b18424ea0c40a9e549ed4183
Fawole Joshua Ajibola <fawolejoshuaajibola@gmail.com>
2022-10-25 02:06:29 +0100

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,6 @@
 int _atoi(char *s)
 {
 int c = 0;
-
 unsigned int ni = 0;
 int min = 1;
 int isi = 0;
@@ -20,10 +19,9 @@ if (s[c] == 45)
 {
 min *= -1;
 }
-while (s[c] >= 48 && s[c] <= 57<S-F9)
+while (s[c] >= 48 && s[c] <= 57)
 {
 isi = 1;
-
 ni = (ni * 10) + (s[c] - '0');
 c++;
 }
